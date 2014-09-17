55a86e651fb689c708018740c8fea5889a83d4b0
Jerry <jerry@ubuntu.(none)>
2014-09-17 08:48:46 -0700

    debugging

--- src/jvm/backtype/storm/scheduler/Elasticity/GetStats.java
+++ src/jvm/backtype/storm/scheduler/Elasticity/GetStats.java
@@ -34,12 +34,12 @@ public class GetStats {
 	protected GetStats() {
 		statsTable = new HashMap<String, Integer>();
 		startTimes = new HashMap<String, Long>();
-		node_stats = new HashMap<String, Integer> ();
-		
+		node_stats = new HashMap<String, Integer>();
+
 	}
 
 	public static GetStats getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new GetStats();
 		}
 		return instance;
@@ -47,19 +47,20 @@ public class GetStats {
 
 	public void getStatistics(String filename) {
 		LOG.info("Getting stats...");
-		
+
 		TSocket tsocket = new TSocket("localhost", 6627);
 		TFramedTransport tTransport = new TFramedTransport(tsocket);
 		TBinaryProtocol tBinaryProtocol = new TBinaryProtocol(tTransport);
 		Nimbus.Client client = new Nimbus.Client(tBinaryProtocol);
-		
+
 		try {
 			tTransport.open();
 			ClusterSummary clusterSummary = client.getClusterInfo();
 			List<TopologySummary> topologies = clusterSummary.get_topologies();
 			for (TopologySummary topo : topologies) {
-				if(this.startTimes.containsKey(topo.get_id()) == false) {
-					this.startTimes.put(topo.get_id(), (System.currentTimeMillis() / 1000));
+				if (this.startTimes.containsKey(topo.get_id()) == false) {
+					this.startTimes.put(topo.get_id(),
+							(System.currentTimeMillis() / 1000));
 				}
 				TopologyInfo topologyInfo = null;
 				try {
@@ -88,44 +89,51 @@ public class GetStats {
 							.get_transferred();
 
 					if (transfer.get(":all-time").get("default") != null) {
-						String hash_id = host + ':' + port + ':' + componentId + ":"
-								+ topo.get_id() + ":" + taskId;
-						Integer totalOutput = transfer
-								.get(":all-time").get("default").intValue();
-						
-						if (this.statsTable.containsKey(hash_id) == false) { 
+						String hash_id = host + ':' + port + ':' + componentId
+								+ ":" + topo.get_id() + ":" + taskId;
+						Integer totalOutput = transfer.get(":all-time")
+								.get("default").intValue();
+
+						if (this.statsTable.containsKey(hash_id) == false) {
 							this.statsTable.put(hash_id, totalOutput);
 						}
-						
-						Integer throughput = totalOutput - this.statsTable.get(hash_id);
+
+						Integer throughput = totalOutput
+								- this.statsTable.get(hash_id);
 						LOG.info((host + ':' + port + ':' + componentId + ":"
-								+ topo.get_id() + ":" + taskId + "," + transfer
-								.get(":all-time").get("default")+","+this.statsTable.get(hash_id)+","+throughput));
-						
+								+ topo.get_id() + ":" + taskId + ","
+								+ transfer.get(":all-time").get("default")
+								+ "," + this.statsTable.get(hash_id) + "," + throughput));
+
 						this.statsTable.put(hash_id, totalOutput);
-						
-						//get node stats
-						if(this.node_stats.containsKey(host) == false) {
+
+						// get node stats
+						if (this.node_stats.containsKey(host) == false) {
 							this.node_stats.put(host, 0);
 						}
-						this.node_stats.put(host, this.node_stats.get(host) + throughput);
+						this.node_stats.put(host, this.node_stats.get(host)
+								+ throughput);
 
-						long unixTime = (System.currentTimeMillis() / 1000) - this.startTimes.get(topo.get_id());
+						long unixTime = (System.currentTimeMillis() / 1000)
+								- this.startTimes.get(topo.get_id());
 						String data = String.valueOf(unixTime) + ':' + host
 								+ ':' + port + ':' + componentId + ":"
 								+ topo.get_id() + ":" + taskId + ","
-								+ throughput
-								+ "\n";
-						
-						 String filePath = "/tmp/"+filename; try {
-						 //LOG.info("writting to file..."); File file = new
-						 File(filePath); FileWriter fileWritter = new
-						 FileWriter(file,true); BufferedWriter bufferWritter =
-						 new BufferedWriter(fileWritter);
-						 bufferWritter.append(data); bufferWritter.close();
-						 fileWritter.close(); } catch(IOException ex) {
-						 LOG.info("error! writin to file {}", ex); }
-						 
+								+ throughput + "\n";
+
+						String filePath = "/tmp/" + filename;
+						try {
+							// LOG.info("writting to file...");
+							File file = new File(filePath);
+							FileWriter fileWritter = new FileWriter(file, true);
+							BufferedWriter bufferWritter = new BufferedWriter(
+									fileWritter);
+							bufferWritter.append(data);
+							bufferWritter.close();
+							fileWritter.close();
+						} catch (IOException ex) {
+							LOG.info("error! writin to file {}", ex);
+						}
 					}
 				}
 				LOG.info("OVERALL THROUGHPUT: {}", this.node_stats);
