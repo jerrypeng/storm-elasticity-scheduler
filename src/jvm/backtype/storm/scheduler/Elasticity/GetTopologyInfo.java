eaf2bbdd0164fcc6a86e0a044652a1cb39e1d3f2
Jerry <jerry@ubuntu.(none)>
2014-11-02 13:21:54 -0800

    experimenting

--- src/jvm/backtype/storm/scheduler/Elasticity/GetTopologyInfo.java
+++ src/jvm/backtype/storm/scheduler/Elasticity/GetTopologyInfo.java
@@ -75,9 +75,7 @@ public class GetTopologyInfo {
 								this.all_comp.put(s.getKey(), newComp);
 							}
 
-							LOG.info("get_common: {}",s.getValue().get_common());
-							LOG.info("Streams: {}",s.getValue().get_common().get_streams());
-							LOG.info("INputs: {}",s.getValue().get_common().get_inputs());
+							
 							for (Map.Entry<GlobalStreamId, Grouping> entry : s
 									.getValue().get_common().get_inputs()
 									.entrySet()) {
@@ -113,12 +111,21 @@ public class GetTopologyInfo {
 								newComp = new Component(s.getKey());
 								this.all_comp.put(s.getKey(), newComp);
 							}
+							LOG.info("get_common: {}",s.getValue().get_common());
+							LOG.info("Streams: {}",s.getValue().get_common().get_streams());
+							for(Map.Entry<String, StreamInfo> entry : s.getValue().get_common().get_streams().entrySet()){
+								LOG.info("Key: {} Value: {}", entry.getKey(), entry.getValue());
+								LOG.info("outputFields: {}", entry.getValue().get_output_fields());
+							
+							}
+							LOG.info("INputs: {}",s.getValue().get_common().get_inputs());
 							for (Map.Entry<GlobalStreamId, Grouping> entry : s
 									.getValue().get_common().get_inputs()
 									.entrySet()) {
 								if (entry.getKey().get_componentId()
 										.matches("(__).*") == false) {
-
+									LOG.info("ComponentID: {} --- {}",entry.getKey().get_componentId(), entry.getValue());
+									
 									newComp.parents.add(entry.getKey()
 											.get_componentId());
 									if (this.all_comp.containsKey(entry
