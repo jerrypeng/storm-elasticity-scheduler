7bedd4d9de7554b2b2778716407c5d1a815592f7
Jerry <jerry@ubuntu.(none)>
2014-11-28 17:20:25 -0800

    testing

--- src/jvm/backtype/storm/scheduler/Elasticity/Strategies/IncreaseParallelismTest.java
+++ src/jvm/backtype/storm/scheduler/Elasticity/Strategies/IncreaseParallelismTest.java
@@ -1,7 +1,10 @@
 package backtype.storm.scheduler.Elasticity.Strategies;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import backtype.storm.scheduler.Cluster;
@@ -23,7 +26,24 @@ public class IncreaseParallelismTest extends TopologyHeuristicStrategy{
 
 	public Map<WorkerSlot, List<ExecutorDetails>> getNewScheduling() {
 		HelperFuncs.changeParallelism(this._topo.getId(), "exclaim2", 4);
-		Map<WorkerSlot, List<ExecutorDetails>> schedMap = this._globalState.schedState.get(this._topo.getId());
+		Map<WorkerSlot, List<ExecutorDetails>> schedMap = new HashMap<WorkerSlot, List<ExecutorDetails>>();
+		for(Map.Entry<WorkerSlot, List<ExecutorDetails>> entry : this._globalState.schedState.get(this._topo.getId()).entrySet()) {
+			schedMap.put(entry.getKey(), new ArrayList<ExecutorDetails>());
+			for(ExecutorDetails exec : entry.getValue()) {
+				if(exec.getStartTask() != exec.getEndTask()) {
+					LOG.info("Exec: {} split into:");
+					for(int i=exec.getStartTask(); i<exec.getEndTask()+1; i++) {
+						ExecutorDetails newExec = new ExecutorDetails(i, i);
+						LOG.info("{} -> {}", i, newExec);
+						schedMap.get(entry.getKey()).add(newExec);
+					}
+				} else {
+					schedMap.get(entry.getKey()).add(exec);
+				}
+				
+			}
+		}
+		LOG.info("schedMap: {}", schedMap);
 		return schedMap;
 	}
 
