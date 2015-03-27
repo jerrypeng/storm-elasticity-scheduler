8f9a297dc8cf95331b8d91c1a0ed1fe5e2224d84
flint-stone <sharonxu65@gmail.com>
2015-03-27 03:32:09 -0500

    debug

--- src/jvm/backtype/storm/scheduler/Elasticity/Strategies/StellaInComplexStrategy.java
+++ src/jvm/backtype/storm/scheduler/Elasticity/Strategies/StellaInComplexStrategy.java
@@ -284,7 +284,7 @@ public class StellaInComplexStrategy extends TopologyHeuristicStrategy {
 		LOG.info("NodeExecutorMap: {}",this.NodeExecutorMap);
 		LOG.info("List of Execs: {}",this.NodeExecutorMap.get(node));
 		for(ExecutorDetails e:this.NodeExecutorMap.get(node)){
-			LOG.info("Executor: {}",e);
+			LOG.info("Executor: {}",e); 
 			LOG.info("belongs to component: {}",getCompByExec(e));
 			Double orig=ExpectedExecuteRateMap.get(getCompByExec(e));
 			ExpectedExecuteRateMap.put(getCompByExec(e), orig-ExecutorExecuteRateMap.get(e));
